cc3813835df897b601afb4289a913a1354ce46fc
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-11-13 12:30:41 +0530

    Initial Setup of Register User

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,10 @@
+const { StatusCodes } = require("http-status-codes");
+const CustomAPIError = require("../errors/custom-api");
+const User = require("../models/User");
+
 const register = async (req, res) => {
-  res.send("User Register ");
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 const login = async (req, res) => {
   res.send("User Login ");
