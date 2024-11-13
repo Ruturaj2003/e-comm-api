5bbe5bfbd8d820386f2d41f9138279eaeca76fbe
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-11-13 11:13:02 +0530

    Add Morgan

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const express = require("express");
 const connectDB = require("./db/connect");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+// Other packages
+const morgan = require("morgan");
+
 // Server Setup
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
+app.use(morgan("tiny"));
 app.use(express.json());
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("E-Comm Server");
+  res.send("E-Comm Server").status(200);
   return;
 });
 
