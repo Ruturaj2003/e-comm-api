0c7af80c1b359e278752db9896413a16d0f017a2
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-11-13 10:41:02 +0530

    Setup Basic Route and Basic Middleware

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 require("dotenv").config();
+require("express-async-errors");
 const express = require("express");
 const connectDB = require("./db/connect");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 // Server Setup
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("E-Comm Server");
+  return;
+});
+
+// Last Middlewares
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
