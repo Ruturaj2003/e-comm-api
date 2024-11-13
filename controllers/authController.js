57ff16204dcf076b06aaba89abefd00985d7f3ec
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-11-13 12:35:53 +0530

    Unique Email Validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,16 @@
 const { StatusCodes } = require("http-status-codes");
-const CustomAPIError = require("../errors/custom-api");
+const CustomError = require("../errors");
 const User = require("../models/User");
 
 const register = async (req, res) => {
+  const { email } = req.body;
+
+  const emailAlreadyExists = User.findOne({ email });
+
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email Already Exists");
+  }
+
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
