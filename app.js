dbad53946bc41a41593e930db28e0714cae95f94
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-11-13 10:32:05 +0530

    Setup Server

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./db/connect");
+// Server Setup
+const app = express();
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Listening on ${port}`));
+  } catch (error) {}
+};
+
+// Start Server
+start();
