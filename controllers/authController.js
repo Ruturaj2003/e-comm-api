c127669fcb345cfbb109617357955e2f5dd12435
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-11-16 12:47:41 +0530

    Setup JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -15,7 +16,14 @@ const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments({})) === 0;
   const role = isFirstAccount ? "admin" : "user";
   const user = await User.create({ name, email, password, role }); // Even if someone tries to pass role , it wont do anything
-  res.status(StatusCodes.CREATED).json({ user });
+
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  // Issue a JWT Token
+  const token = jwt.sign(tokenUser, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
 };
 const login = async (req, res) => {
   res.send("User Login ");
