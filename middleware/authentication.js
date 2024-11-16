72c7e1b14eb734be2c6486344dcb246060a7aca2
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-11-16 20:29:28 +0530

    Setup Role Based Access

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -20,6 +20,18 @@ const authenticateUser = async (req, res, next) => {
   }
 };
 
+const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError(
+        "Only Admins Can Request this resource"
+      );
+    }
+    next();
+  };
+};
+
 module.exports = {
   authenticateUser,
+  authorizePermissions,
 };
