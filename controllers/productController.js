79d01d8cc256131e4676a62e6591f9cfe4c04f9b
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-11-19 21:27:57 +0530

    Setup CURD operations for Product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,59 @@
-const createProduct = (req, res) => {
-  res.send("Create Product Function Called");
+const { StatusCodes } = require("http-status-codes");
+const Product = require("../models/Product");
+const CustomError = require("../errors");
+
+const createProduct = async (req, res) => {
+  req.body.user = req.user.userId;
+  const product = await Product.create(req.body);
+  res.status(StatusCodes.CREATED).json({ product });
 };
-const getAllProducts = (req, res) => {
-  res.send("Get All Products Function Called");
+const getAllProducts = async (req, res) => {
+  const products = await Product.find({});
+
+  res.status(StatusCodes.OK).json({ products, count: products.length });
 };
-const getSingleProduct = (req, res) => {
+const getSingleProduct = async (req, res) => {
+  const { id: productId } = req.params;
+
+  const product = await Product.findOne({
+    _id: productId,
+  });
+
+  res.status(StatusCodes.OK).json(product);
   res.send("Get Single Product Function Called");
 };
-const updateProduct = (req, res) => {
-  res.send("Update Product Function Called");
+const updateProduct = async (req, res) => {
+  const { id: productId } = req.params;
+
+  const product = await Product.findOneAndUpdate(
+    {
+      _id: productId,
+    },
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  if (!product) {
+    throw new CustomError.NotFoundError("No such Product");
+  }
+
+  res.status(StatusCodes.OK).json(product);
 };
-const deleteProduct = (req, res) => {
-  res.send("Delete Product Function Called");
+const deleteProduct = async (req, res) => {
+  const { id: productId } = req.params;
+  const product = await Product.findOne({
+    _id: productId,
+  });
+
+  if (!product) {
+    throw new CustomError.NotFoundError("No such Product");
+  }
+
+  await product.remove();
+  res.status(StatusCodes.OK).json("Deleted");
 };
 const uploadImage = (req, res) => {
   res.send("Upload Image Function Called");
