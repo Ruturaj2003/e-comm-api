b8c313e464a4fd78e5a57209a0782c5077efc086
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-11-20 20:18:11 +0530

    Setup Review Model

---
+++ models/Review.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, "Please Provide a Rating"],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "Please Provide a Title"],
+    },
+    comment: {
+      type: String,
+      required: [true, "Please Provide review text"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+module.exports = mongoose.model("Review", ReviewSchema);
