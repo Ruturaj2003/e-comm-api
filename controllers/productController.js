c40931fd714886e891ba0ff0741ea35f43021ffc
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-11-24 19:05:27 +0530

    Setup Mongoose Virtual

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,7 +9,7 @@ const createProduct = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ product });
 };
 const getAllProducts = async (req, res) => {
-  const products = await Product.find({});
+  const products = await Product.find({}).populate("reviews");
 
   res.status(StatusCodes.OK).json({ products, count: products.length });
 };
