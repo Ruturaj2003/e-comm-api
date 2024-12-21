04ec5de0909e5521f8122434afc3fc0c4827a464
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-12-21 21:32:59 +0530

    Implement Create Order Controller

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,73 @@
+const Product = require("../models/Product");
+const Order = require("../models/Order");
+
+const checkPermissions = require("../utils/checkPermissions");
+const CustomError = require("../errors");
+const { StatusCodes } = require("http-status-codes");
+
+const fakeStripeAPI = async ({ amount, currency }) => {
+  const client_secret = "DotaizzOP";
+  return { client_secret, amount };
+};
+
 const createOrder = async (req, res) => {
-  res.send("create order");
+  const { items: cartItems, tax, shippingFee } = req.body;
+
+  if (!cartItems || cartItems.lenght < 1) {
+    throw new CustomError.BadRequestError("No Cart Items");
+  }
+
+  if (!tax || !shippingFee) {
+    throw new CustomError.BadRequestError("Please Provide Tax And Shiping Fee");
+  }
+
+  let orderItems = [];
+  let subtotal = 0;
+
+  // For of Loop bcz it supports await in it
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product });
+
+    if (!dbProduct) {
+      throw new CustomError.NotFoundError("No such Product " + item.product);
+    }
+
+    const { name, price, image, _id } = dbProduct;
+
+    console.log(name, price, image);
+
+    const singleOrderItem = {
+      amount: item.amount,
+      name,
+      price,
+      image,
+      product: _id,
+    };
+    // Add items to order
+
+    orderItems = [...orderItems, singleOrderItem];
+    // Calc sub total
+    subtotal += price * item.amount;
+    console.log(subtotal);
+  }
+  const total = tax + shippingFee + subtotal;
+  // Get CLient Sec
+  const paymentIntent = await fakeStripeAPI({
+    amount: total,
+    currency: "USD",
+  });
+
+  const order = await Order.create({
+    orderItems,
+    total,
+    subtotal,
+    tax,
+    shippingFee,
+    clientSecret: paymentIntent.client_secret,
+    user: req.user.userId,
+  });
+
+  res.status(StatusCodes.CREATED).json({ clientSecret: order.clientSecret });
 };
 const getAllOrders = async (req, res) => {
   res.send("get all order");
